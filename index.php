9c952e9c61135ec2e5d0de12172cb0e6f281bb17
Michał Moroz <michalmoroz@gmail.com>
2013-05-12 16:57:09 +0200

    added wampir

--- index.php
+++ index.php
@@ -116,17 +116,84 @@
         </div>
     </div>
     
-    <a class="button" href="#dontforget"><i class="icon-arrow-down"></i> Pamiętaj także o</a>
+    <a class="button" href="#dontforget"><i class="icon-arrow-down"></i> Czas na fun</a>
 </section>
 <section id="dontforget">
     <div class="grid">
+            <header>
+                <h2>To nie wszystko!</h2>
+                <p>Zobacz, co jeszcze dla Ciebie przygotowaliśmy.</p>
+            </header>
+
+            <div class="row">
+                <div class="slot-0-1-2">
+                    <p class="center"><img src="<?php echo App::path('img/wampir.png') ?>" alt=""></p>
+                    <h3>Gifty</h3>
+                    <p>W zamian za oddanie krwi otrzymasz:</p>
+                    <ul>
+                        <li>osiem czekolad;</li>
+                        <li>batonik i napój energetyczny;</li>
+                        <li>koszulkę Wampira;</li>
+                        <li>zniżkę na pizzę Fiero;</li>
+                        <li>oraz zaproszenie na WAMPIRPARTY.</li>
+                    </ul>
+                </div>
+                <div class="slot-3-4-5">
+                    <p class="lordis center"><img src="<?php echo App::path('img/lordis.png') ?>" alt="Logo Lordi's Club"></p>
+                    <h3>Impreza</h3>
+                    <p>Już 30 maja w Lordi's Club przy ul. Piotrkowskiej 102 rozlegną się dźwięki WAMPIRPARTY, imprezy kierowanej do wszystkich honorowych krwiodawców, którzy oddali swoją krew podczas tej edycji Wampiriady. Przyjdź i weź ze sobą znajomych.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="slot-0-1-2">
+                    <h3>Polub nas na Facebooku,</h3>
+                    <p>a weźmiesz udział w losowaniu nagród takich jak karnety do Fitfabric Fitness Club, kursów językowych Profilingua, czy darmowych kuponów na pizzę w pizzerii Fiero. Losowanie odbywa się po każdej akcji Wampiriady!</p>
+                    <p>Nasz fanpage: <a href="https://www.facebook.com/NZSRegionuLodzkiego">NZS Regionu Łódzkiego</a>.</p>
+                </div>
+                <div class="slot-3-4-5">
+                    <h3>Pozostań w kontakcie</h3>
+                    <p>Najświeższe informacje o naszych projektach możesz zdobyć tutaj:</p>
+                    <ul>
+                        <li><a href="https://www.facebook.com/NZSRegionuLodzkiego">NZS Regionu Łódzkiego</a></li>
+                        <li><a href="https://www.facebook.com/wampiriada.nzs.rl">Wampiriada NZS RŁ</a></li>
+                        <li><a href="https://www.facebook.com/events/108792442658412/">Oficjalny event 22 edycji Wampiriady</a></li>
+                        <li><a href="https://twitter.com/nzslodz">Nasz Twitter</a></li>
+                    </ul>
+                </div>
+            </div>
     </div>
+    <a class="button" href="#who"><i class="icon-arrow-down"></i> Kto za tym stoi?</a>
 </section>
 <section id="who">
     <div class="grid">
+            <header>
+                <h2>Organizatorzy</h2>
+                <p>Zajrzyj na nasze strony, jeśli podoba Ci się nasza akcja.</p>
+            </header>
+
+            <div class="row">
+            <div class="slot-0-1-2">
+                <a href="http://nzs.lodz.pl"><img src="<?php echo App::path('img/nzs.png') ?>" alt="Niezależne Zrzeszenie Studentów Regionu Łódzkiego"></a>
+            </div>
+            <div class="slot-3-4-5">
+                <a href="http://krwiodawstwo.pl"><img src="<?php echo App::path('img/rck.png') ?>" alt="Regionalne Centrum Krwiodawstwa i Krwiolecznictwa w Łodzi"></a>
+            </div>
+            </div>
     </div>
+
+    <a class="button" href="#partners"><i class="icon-arrow-down"></i> Nasi partnerzy</a>
 </section>
 <section id="partners">
     <div class="grid">
+            <header>
+                <h2>Partnerzy</h2>
+                <p>Serdecznie dziękujemy naszym partnerom za możliwość przeprowadzenia akcji Wampiriada w Łodzi.</p>
+            </header>
+            
+            <div class="row">
+                <img src="<?php echo App::path('img/logotypyweb.png') ?>" alt="Logotypy naszych partnerów">
+            </div>
     </div>
+    <a class="button" href="#intro"><i class="icon-arrow-up"></i> Powrót na górę</a>
 </section>
