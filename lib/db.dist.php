dc73b60c47c792fae1d4d8727114c2a5675b2958
Michał Moroz <michalmoroz@gmail.com>
2014-02-19 15:28:42 +0100

    added wampi

--- lib/db.dist.php
+++ lib/db.dist.php
@@ -1,1 +1,1 @@
-<?php return new PDO('mysql:dbname=wampiriada_backend;host=localhost', 'root', 'root');
+<?php return new PDO('mysql:dbname=wampiriada_backend;host=localhost', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
