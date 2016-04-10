6399afd6aa32aa56f0d188c4b3b8828b154c4f65
Michał Moroz <michalmoroz@gmail.com>
2016-04-10 22:00:25 +0200

    Dotenv does not fire on production

--- src/Controller.php
+++ src/Controller.php
@@ -12,10 +12,12 @@ class Controller {
         date_default_timezone_set("Europe/Warsaw");
 
         // Load database configuration through Dotenv
-        $dotenv = new Dotenv(__DIR__ . '/..');
-        $dotenv->load();
+        if(getenv('ENVIRONMENT') !== 'production') {
+            $dotenv = new Dotenv(__DIR__ . '/..');
+            $dotenv->load();
 
-        $dotenv->required(['DB_DRIVER', 'DB_HOST', 'DB_USERNAME', 'DB_PASSWORD', 'DB_NAME']);
+            $dotenv->required(['DB_DRIVER', 'DB_HOST', 'DB_USERNAME', 'DB_PASSWORD', 'DB_NAME']);
+        }
 
         // Create new Laravel Capsule and start Eloquent
         $this->capsule = new Capsule;
