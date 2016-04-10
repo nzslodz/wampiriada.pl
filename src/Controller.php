52bcdf5babeab9fa6b8ac5b4e4a1014172ab6128
Michał Moroz <michalmoroz@gmail.com>
2016-04-10 21:52:47 +0200

    removed old configuration in favour of dotenv

--- src/Controller.php
+++ src/Controller.php
@@ -2,16 +2,34 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use NZS\Wampiriada\EditionRepository;
+use Silverplate\App;
+use Dotenv\Dotenv;
 
 class Controller {
     protected $capsule, $editions = array();
     
     public function __construct() {
         date_default_timezone_set("Europe/Warsaw");
-        
+
+        // Load database configuration through Dotenv
+        $dotenv = new Dotenv(__DIR__ . '/..');
+        $dotenv->load();
+
+        $dotenv->required(['DB_DRIVER', 'DB_HOST', 'DB_USERNAME', 'DB_PASSWORD', 'DB_NAME']);
+
+        // Create new Laravel Capsule and start Eloquent
         $this->capsule = new Capsule;
 
-        $this->capsule->addConnection(require __DIR__ . '/../config/db.php');
+        $this->capsule->addConnection([
+            'driver' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'database' => getenv('DB_NAME'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_polish_ci',
+            'prefix' => '',
+        ]);
 
         $this->capsule->bootEloquent();
     }
