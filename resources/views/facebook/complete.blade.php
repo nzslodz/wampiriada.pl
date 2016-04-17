6808fdf643c6e6d43a29f09a62f0ab095baee08d
Michał Moroz <michalmoroz@gmail.com>
2016-04-17 17:30:17 +0200

    fix

--- resources/views/facebook/complete.blade.php
+++ resources/views/facebook/complete.blade.php
@@ -4,8 +4,14 @@
     <div class="container">
         <div class="page-header">
             <h1>Dziękujemy!</h1>
-            <p>Wylogowaliśmy Cię z facebooka automatycznie.</p>
-        </div>     
+            <p>Wylogowaliśmy Cię z Facebooka automatycznie. Nic więcej nie musisz robić. :) Ta strona przeładuje się samoczynnie za 10 sekund.</p>
+        </div> 
+
+        <script type="text/javascript">
+            setTimeout(function() {
+                location.href = '{{ url('facebook/login') }}'
+            }, 10000)
+        </script>
     </div>
 
 
