a7e71c29f901f1b34d6ad254ce851b9fcb489d51
Michał Moroz <michalmoroz@gmail.com>
2016-06-21 01:03:10 +0200

    small fixes usability

--- app/Http/Controllers/PrizeController.php
+++ app/Http/Controllers/PrizeController.php
@@ -27,7 +27,7 @@ class PrizeController extends Controller {
         $edition_number = (int) Option::get('wampiriada.edition', 26);
 
         return view('admin.prize.list', [
-			'prizes' => PrizeType::sortBy('name')->get(),
+			'prizes' => PrizeType::orderBy('name')->get(),
 		]);
 	}
 
