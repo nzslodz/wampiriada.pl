003497779c2edbc6993a110eb8d2d36a27f6b152
Michał Moroz <michalmoroz@gmail.com>
2016-11-08 00:32:40 +0000

    magic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          Commands\ImportExcelData::class,
-         Commands\CreateImageGrid::class,
          Commands\RegenerateTileImage::class,
          Commands\DispatchWampiriadaMailing::class,
     ];
