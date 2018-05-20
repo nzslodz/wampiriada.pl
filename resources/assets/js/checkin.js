9cac5cd6c8af7d94bc3f8355f9bc26fccbed5106
Michał Moroz <michalmoroz@gmail.com>
2018-05-20 12:08:23 +0200

    Statistics view

--- resources/assets/js/checkin.js
+++ resources/assets/js/checkin.js
@@ -9,18 +9,51 @@ require('./bootstrap');
 
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Vuelidate from 'vuelidate'
+
 import { mapState } from 'vuex'
 
+Vue.use(Vuelidate)
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
         'currentView': 0,
-        'tshirtSizes': shirtSizes,
+        'staticData': {
+            'shirtSizes': shirtSizes,
+            'bloodTypes': {
+                'a_plus': 'A+',
+                'a_minus': 'A-',
+                'b_plus': 'B+',
+                'b_minus': 'B-',
+                'ab_plus': 'AB+',
+                'ab_minus': 'AB-',
+                'zero_plus': '0+',
+                'zero_minus': '0-',
+                'unknown': 'Nie wiem'
+            }
+        },
+        'userInput': {
+            'bloodType': null,
+            'chosenSize': null,
+            'firstTime': false,
+        }
     },
     mutations: {
         'nextStep': state => state.currentView++,
         'previousStep': state => state.currentView--,
+
+        setShirtSize(state, value) {
+            state.userInput.chosenSize = value
+        },
+
+        setBloodType(state, value) {
+            state.userInput.bloodType = value
+        },
+
+        setFirstTime(state, value) {
+            state.userInput.firstTime = value
+        },
     }
 });
 
--- resources/assets/js/components/Checkin/Statistics.vue
+++ resources/assets/js/components/Checkin/Statistics.vue
@@ -4,35 +4,31 @@
             <h3>Dane statystyczne</h3>
             <div class="form-group">
                 <label for="">Oddaję (pierwszy raz/kolejny raz)</label>
-                <button type="button" class="btn btn-lg btn-toggle" data-toggle="button" aria-pressed="false" autocomplete="off">
+                <button type="button" :class="['btn', 'btn-lg', 'btn-toggle', {'active': firstTime}]" v-bind:aria-pressed="firstTime ? 'true' : 'false'" autocomplete="off" @click="setFirstTime()">
                     <div class="handle"></div>
                 </button>
             </div>
 
-            <!-- has-error -->
-            <div class="form-group">
+            <div :class="['form-group', {'has-error': $v.bloodType.$error}]">
                 <label for="blood_type" class="control-label">Grupa krwi</label>
-                <select name="blood_type" id="blood_type" class="form-control">
-                    <option>---</option>
-                    <option value="a_plus">A+</option>
-                    <option value="a_minus">A-</option>
-                    <option value="b_plus">B+</option>
-                    <option value="b_minus">B-</option>
-                    <option value="ab_plus">AB+</option>
-                    <option value="ab_minus">AB-</option>
-                    <option value="zero_plus">0+</option>
-                    <option value="zero_minus">0-</option>
-                    <option value="unknown">Nie wiem</option>
+                <select name="blood_type" id="blood_type" class="form-control" v-model="bloodType" @input="$v.bloodType.$touch()">
+                    <option value>---</option>
+                    <option v-for="(name, id) in staticBloodTypes" :value="id">{{ name }}</option>
                 </select>
+                <p class="help-block" v-if="!$v.bloodType.required && $v.bloodType.$error">To pole jest wymagane</p>
             </div>
 
-            <div class="form-group">
+            <div :class="['form-group', {'has-error': $v.shirtSize.$error}]">
                 <label class="control-label" for="size">Rozmiar koszulki</label>
-                <select name="size" id="size" class="form-control">
-                    <option v-for="(name, id) in tshirtSizes" :value="id">{{ name }}</option>
+                <select name="size" id="size" class="form-control" v-model.number="shirtSize" @input="$v.shirtSize.$touch()">
+                    <option value>---</option>
+                    <option v-for="(name, id) in staticShirtSizes" :value="id">{{ name }}</option>
                 </select>
+                <p class="help-block" v-if="!$v.shirtSize.required && $v.shirtSize.$error">To pole jest wymagane</p>
              </div>
-            <button type="button" v-on:click="nextStep()">yo</button>
+            <button type="button" class="btn btn-default btn-primary" :disabled="$v.$invalid" v-on:click="nextStep()">
+                {{ $v.$invalid ? 'Wypełnij wszystkie pola': 'Przejdź dalej' }}
+            </button>
         </div>
     </section>
 </template>
@@ -40,12 +36,55 @@
 <script>
     import { default as mixins } from './mixins';
     import { mapState } from 'vuex'
+    import { required } from 'vuelidate/lib/validators'
+
 
     export default {
         mixins: [mixins],
 
         computed: {
-            ...mapState(['tshirtSizes'])
+            ...mapState({
+                staticShirtSizes: state => state.staticData.shirtSizes,
+                staticBloodTypes: state => state.staticData.bloodTypes
+            }),
+
+            bloodType: {
+                get() {
+                    return this.$store.state.userInput.bloodType
+                },
+                set(value) {
+                    this.$store.commit('setBloodType', value === '' ? null: value)
+                }
+            },
+
+            shirtSize: {
+                get() {
+                    return this.$store.state.userInput.chosenSize
+                },
+                set(value) {
+                    this.$store.commit('setShirtSize', value === '' ? null: value)
+                }
+            },
+
+            firstTime() {
+                return this.$store.state.userInput.firstTime
+            },
+        },
+
+        methods: {
+            setFirstTime() {
+                this.$store.commit('setFirstTime', !this.$store.state.userInput.firstTime)
+            }
+        },
+
+        validations: {
+            bloodType: {
+                required,
+            },
+
+            shirtSize: {
+                required,
+            }
         },
 
         mounted() {
